61a41b8ad6168398e8954bc097f9e68d4bc87626
xxbatokxx <iqbalrisqi700@gmail.com>
2023-12-02 03:35:45 +0700

    env

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
+require('dotenv').config()
+const PORT = process.env.PORT || 5000
 const express = require('express');
 const app = express();
 const foodRoutes = require('./routes/foods')
 
 
+
 app.use('/foods', foodRoutes);
 
 
-app.listen(4000, () => {
-    console.log('server berhasil di running di port 4000');
+app.listen(PORT, () => {
+    console.log(`Server berjalan di port ${PORT}`);
 })
