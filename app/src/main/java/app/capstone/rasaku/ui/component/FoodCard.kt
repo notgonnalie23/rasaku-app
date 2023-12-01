ebe8cbf152f995972acd99b1cfb9540521d73d69
reviwh <reviwh2@gmail.com>
2023-12-01 18:43:57 +0700

    Adding Food Card component

---
+++ app/src/main/java/app/capstone/rasaku/ui/component/FoodCard.kt
@@ -0,0 +1,67 @@
+package app.capstone.rasaku.ui.component
+
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import app.capstone.rasaku.R
+import app.capstone.rasaku.ui.theme.RasakuTheme
+import coil.compose.AsyncImage
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun FoodCard(
+    name: String,
+    imageUrl: String,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    ElevatedCard(
+        onClick = onClick,
+        modifier = modifier.width(96.dp)
+    ) {
+        FoodCardImage(
+            imageUrl = imageUrl,
+            modifier = modifier.height(109.dp)
+        )
+        Text(
+            text = name,
+            style = MaterialTheme.typography.bodyLarge,
+            modifier = modifier.padding(8.dp),
+            overflow = TextOverflow.Ellipsis,
+            maxLines = 2,
+        )
+    }
+}
+
+@Composable
+private fun FoodCardImage(
+    imageUrl: String,
+    modifier: Modifier = Modifier,
+) {
+    AsyncImage(
+        model = imageUrl,
+        contentDescription = null,
+        placeholder = painterResource(id = R.drawable.placeholder_card),
+        contentScale = ContentScale.Crop,
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun FoodCardPreview() {
+    RasakuTheme {
+        FoodCard(name = "Food Name", imageUrl = "", onClick = { })
+    }
+}
