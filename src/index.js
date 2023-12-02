b99e101a2f63421305225d5985545240616d2c4a
xxbatokxx <iqbalrisqi700@gmail.com>
2023-12-02 22:54:25 +0700

    Get API idfood

--- src/index.js
+++ src/index.js
@@ -3,8 +3,10 @@ const PORT = process.env.PORT || 5000
 const express = require('express');
 const app = express();
 const foodRoutes = require('./routes/foods')
+const middlewareLog = require('./middleware/log')
 
-
+app.use(middlewareLog)
+app.use(express.json())
 
 app.use('/foods', foodRoutes);
 
