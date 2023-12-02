1a091ed894434637f32993e71399ada1f066ba61
reviwh <reviwh2@gmail.com>
2023-12-02 23:13:18 +0700

    Minor fixing (search screen)

--- app/src/main/java/app/capstone/rasaku/ui/screen/search/SearchScreen.kt
+++ app/src/main/java/app/capstone/rasaku/ui/screen/search/SearchScreen.kt
@@ -1,11 +1,10 @@
 package app.capstone.rasaku.ui.screen.search
 
-import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import app.capstone.rasaku.ui.component.CategorySection
 import app.capstone.rasaku.ui.component.Header
 import app.capstone.rasaku.ui.theme.RasakuTheme
@@ -13,35 +12,35 @@ import app.capstone.rasaku.ui.theme.RasakuTheme
 @Composable
 fun SearchScreen(
     navigateToSearch: () -> Unit,
-    modifier : Modifier = Modifier
-){
+    modifier: Modifier = Modifier
+) {
     SearchContent(
         imageUrl = "https://placehold.co/48x48/png",
         navigateToSearch = navigateToSearch,
         modifier = modifier,
-        )
+    )
 }
 
 @Composable
 private fun SearchContent(
     imageUrl: String,
     navigateToSearch: () -> Unit,
-    modifier: Modifier,
+    modifier: Modifier = Modifier,
 ) {
-    LazyColumn(
-        verticalArrangement = Arrangement.spacedBy(16.dp)
-    ){
-        item{
+    Column {
         Header(imageUrl = imageUrl, navigateToSearchInput = navigateToSearch)
-        }
-        items(4){ index ->
-            CategorySection(title = "Section #$index")
+        LazyColumn {
+            item {
+            }
+            items(4) { index ->
+                CategorySection(title = "Section #$index")
+            }
         }
     }
 }
 
 @Preview(showBackground = true)
 @Composable
-private fun SearchScreenPreview(){
-    RasakuTheme { SearchScreen({})}
+private fun SearchScreenPreview() {
+    RasakuTheme { SearchScreen({}) }
 }
