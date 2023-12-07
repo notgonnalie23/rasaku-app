263d9ee74760bf2d4cbc261a3b4cf54e316ca744
xxbatokxx <iqbalrisqi700@gmail.com>
2023-12-07 23:54:58 +0700

    init

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,10 @@
 const mysql = require('mysql2');
 const conn = mysql.createConnection({
-  host: `/cloudsql/${process.env.DB_HOST}`,
+  host: process.env.DB_HOST,
   user: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  socketPath: `/cloudsql/${process.env.INSTANCE_CONNECTION_NAME}`
 });
 
 module.exports = conn.promise();
