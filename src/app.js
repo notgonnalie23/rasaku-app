cf6959edafc8db00bf1636e0e75280d7beb2e7a3
Kristianto <engkuskris5@gmail.com>
2023-12-12 16:03:30 +0700

    rekomendasi tempat jualan berdasarkan lokasi user -gmaps

--- src/app.js
+++ src/app.js
@@ -1,38 +1,34 @@
-// app.js
 const express = require('express');
-const geolocationController = require('./controllers/geolocationController');
-const { getAddressFromCoordinates, recommendNearbyRestaurants } = require('./services/googleMapsService');
-const { formatDate } = require('./utils/helperFunctions');
+const axios = require('axios');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = 5000;
 
-app.use('/api', geolocationController);
+app.use(express.json());
 
+app.post('/getNearbyPlaces', async (req, res) => {
+  try {
+    const { latitude, longitude, keyword } = req.body;
 
-const coordinates = { latitude: -7.303241199723871, longitude: 110.0044145 };
+   
+    const apiKey = 'AIzaSyCQK3Tvf9-yQpC7yBQN8u2N2O3xbLDJDkQ';
+    const radius = 30000; // Radius pencarian dalam meter
+    const types = 'restaurant';
 
-// mendapatkan daftar restoran terdekat
-recommendNearbyRestaurants(coordinates.latitude, coordinates.longitude)
-  .then((restaurants) => {
-    console.log('Rekomendasi Restoran Terdekat:', restaurants);
-  })
-  .catch((error) => {
+    const placesApiUrl = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${latitude},${longitude}&radius=${radius}&types=${types}&keyword=${keyword}&key=${apiKey}`;
+    
+    const response = await axios.get(placesApiUrl);
+    const places = response.data.results;
+ 
+    // rekomendasi 10 tempat berdasarkan lokasi
+    const recommendedPlaces = places.slice(0, 10);
+
+    res.json({ recommendedPlaces });
+  } catch (error) {
     console.error('Error:', error.message);
-  });
-
-// mendapatkan alamat dari koordinat
-getAddressFromCoordinates(coordinates.latitude, coordinates.longitude)
-  .then((address) => {
-    console.log('Alamat dari koordinat:', address);
-  })
-  .catch((error) => {
-    console.error('Error:', error.message);
-  });
-
-const currentDate = new Date();
-const formattedDate = formatDate(currentDate);
-console.log('Tanggal diformat:', formattedDate);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
