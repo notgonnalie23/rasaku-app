d42ac2fab5abf19a1ef4cccd9c0b5b27336c3bf3
xxbatokxx <iqbalrisqi700@gmail.com>
2023-12-21 21:47:54 +0700

    commit

--- src/controller/imagesController.js
+++ src/controller/imagesController.js
@@ -8,14 +8,15 @@ const bucket = storage.bucket('images_user_request');
 const uploadImage = async (req, res) => {
   try {
     await processFile(req);
+    const originalname = req.file.originalname;
 
-    if (!req.file || !req.file.originalname) {
+    if (!req.file) {
       return res.status(400).send({
           message: 'Please upload a file'
       });
   }
 
-    const blob = bucket.file(req.file.originalname);
+    const blob = bucket.file(originalname);
     const blobStream = blob.createWriteStream({
       resumable: false,
     });
@@ -36,16 +37,16 @@ const uploadImage = async (req, res) => {
         
         try {
           // make the file to public
-          await bucket.file(req.file.originalname).makePublic();
+          await bucket.file(originalname).makePublic();
         } catch {
           return res.status(500).send({
-            message: `Uploaded the file successfully: ${req.file.originalname}, but public access is denied!`,
+            message: `Uploaded the file successfully: ${originalname}, but public access is denied!`,
             url_images: publicUrl,
           });
         }
 
         res.status(200).send({
-          message: "Uploaded the file successfully: " + req.file.originalname,
+          message: "Uploaded the file successfully: " + originalname,
           url_images: publicUrl,
         });
       });
@@ -53,7 +54,7 @@ const uploadImage = async (req, res) => {
     blobStream.end(req.file.buffer);
   } catch (err) {
     res.status(500).send({
-      message: `Could not upload the file: ${req.file.originalname}. ${err}`,
+      message: `Could not upload the file: ${originalname}. ${err}`,
     });
   }
 };
