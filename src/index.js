88963fe3eaa4c61969292571ab85171a0e395e92
xxbatokxx <iqbalrisqi700@gmail.com>
2023-12-21 13:21:28 +0700

    "post images, err bug"

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 require('dotenv').config();
 const PORT = process.env.PORT || 4000;
 const express = require('express');
+const cors = require('cors')
 const app = express();
 const foodRoutes = require('./routes/foods');
-const middlewareLog = require('./middleware/log');
+const imagesRoute = require('./routes/imagesRoute')
+const bodyParser = require('body-parser')
 
 
-app.use(middlewareLog);
+app.use(cors())
 app.use(express.json());
+app.use(bodyParser.json());
 
 app.use('/foods', foodRoutes);
+app.use('/uploadImage', imagesRoute);
 
 
 app.listen(PORT, () => {
